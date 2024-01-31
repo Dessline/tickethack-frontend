37c3ccc1e34d324b88524bb45752a6e0727dbeb8
Dessline <desailles.dessline@outlook.fr>
2024-01-31 17:07:01 +0100

    last changes

--- cart.js
+++ cart.js
@@ -2,20 +2,54 @@ fetch('http://localhost:3000/carts')
 	.then(response => response.json())
 	.then(data => {
         console.log(data);
-		//if (data.cart.length > 0) {
+		if (data.result) {
             for (let i =0; i < data.cart.length; i++) {
-            document.querySelector('#cart').innerHTML += `<div class="cart-trip">
-            <p>${data.cart[i].travel.departure} > ${data.cart[i].travel.arrival}</p>
-            </br>
-            <p>${new Date(data.cart[i].travel.date).getHours()}:${new Date(data.cart[i].travel.date).getMinutes()}</p>
-            </br>
-            <p>${data.cart[i].travel.price}</p>
-            </br>
-            <button class="delete-btn">X</button>
-        </div>
-        </br>`
+            document.querySelector('#cart').innerHTML += `<div class="cart-trip" id=${data.cart[i].travel._id}>
+               <p>${data.cart[i].travel.departure} > ${data.cart[i].travel.arrival}</p>
+               </br>
+               <p>${new Date(data.cart[i].travel.date).getHours()}:${new Date(data.cart[i].travel.date).getMinutes()}</p>
+                </br>
+                <p>${data.cart[i].travel.price}</p>
+                </br>
+                <button class="delete-btn">X</button>
+            </div>
+            </br>`
+            
+
+        } 
+        document.querySelector('#total').textContent = String(data.cart.length);
+        updateDeleteCartEventListener();
+        } else {
+            document.querySelector('#body-container').innerHTML = `<div id="empty-cart-container">
+            <div id="cart-message">
+                <h4>No trip in your cart.</h4></br>
+                <h4>Why not plan a trip ?</h4>
+            </div>
+        </div>`
+        document.querySelector('#total').textContent = '0';
         }
-      //  } else {
-      //      document.querySelector('#cart').innerHTML += `<p>Error</p>`
-      //  }
 	});
+
+  function updateDeleteCartEventListener() {
+
+  for (let i = 0; i < document.querySelectorAll('.delete-btn').length; i++) {
+    let travel = document.querySelectorAll(".cart-trip")[i].id;
+		document.querySelectorAll('.delete-btn')[i].addEventListener('click', function () {
+      fetch('http://localhost:3000/carts', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({travel})
+      })
+				.then(response => response.json())
+				.then(data => {
+          this.parentNode.remove();
+          console.log(data);
+        })
+    })
+}
+}
+
+
+
+
+
